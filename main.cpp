e0e7e388d1f9f9fd74bc55c3218afb713ac3e226
LeeHemingway <coherence0815@gmail.com>
2023-04-05 20:46:02 +0900

    change file path

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 int main(int argc, char** argv)
 {
   // Read the image
-  Mat image = imread("./input.jpg");
+  Mat image = imread("/home/runner/work/cpp-3rd-opencv/cpp-3rd-opencv/input.jpg");
 
   // Resize the image
   Mat resizedImage;
